7d88387de581090d4ecb4e775a8607fe20ce1578
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-06-17 21:15:01 -0400

    completed_task span

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,17 @@ import './App.css';
 
 const tasks = [
 {
-id: 3,
-title: "Order Release",
-desciption: "Check out customers accounts and release or block orders accordingly."
+  id: 3,
+  title: "Order Release",
+  desciption: "Check out customers accounts and release or block orders accordingly."
+  completed: true
 },
 {
   id: 4,
   title: "Weekly Reports",
   desciption: "Sending the weekly reports for overdue invoices."
-  },
+  completed: false  
+},
 ]
 
 class App extends Component {
@@ -22,8 +24,37 @@ class App extends Component {
       viewCompleted:false,
       taskList:tasks
 
-    }
+    };
   }
+
+  displayCompleted = status => {
+    if (status) {
+      return this.setstatus({ viewCompleted: true })
+    }
+    return this.setstatus({ viewCompleted: false })
+  }
+  
+  renderTabList = () => {
+    return (
+      <div className="my-5 tab-list">
+        <span
+          onClick={() => this.displayCompleted(true)}
+          className={this.state.viewCompleted ? "active" : ""}
+        >
+          Completed
+            </span>
+        <span
+          onClick={() => this.displayCompleted(false)}
+          className={this.displayCompleted ? "" : "active"}
+        >
+          Incompleted
+            </span>
+      </div>
+    )
+  }
+
+
+
 }
 
 export default App;
