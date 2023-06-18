a4a01528f87b81ec9dad7f71307f662de0059d1e
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-06-18 19:56:03 -0400

    render 'add-task' section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,13 @@ const tasks = [
 {
   id: 3,
   title: "Order Release",
-  desciption: "Check out customers accounts and release or block orders accordingly."
+  desciption: "Check out customers accounts and release or block orders accordingly.",
   completed: true
 },
 {
   id: 4,
   title: "Weekly Reports",
-  desciption: "Sending the weekly reports for overdue invoices."
+  desciption: "Sending the weekly reports for overdue invoices.",
   completed: false  
 },
 ]
@@ -53,6 +53,35 @@ class App extends Component {
     )
   }
 
+// Rendering items in the list (completed || incompleted)
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.taskList.filter(
+      item => item.completed ==viewCompleted
+    );
+  };
+
+  render() {
+    return (
+      <main className="context">
+        <h1 className="text-black text-uppercase text-center my-4"> Task Manager </h1>
+        <div className="row">
+          <div className="col-md-6 col-sma-10 mx-auto p-0">
+            <div className="card p-3">
+              <div>
+                <button className="btn btn-warning">Add Task</button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </main>
+    )
+  }
 
 
 }
