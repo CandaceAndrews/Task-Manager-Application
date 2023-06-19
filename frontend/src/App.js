a922acf8c87b216108373ace6bec7719dbd7a42a
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-06-19 18:19:38 -0400

    css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
 
-
 const tasks = [
-{
-  id: 1,
-  title: "Call Clients",
-  desciption: "Call clients for overdue invoices.",
-  completed: true
-},
   {
-  id: 2,
-  title: "Dunning",
-  desciption: "Sending dunning letters to clients for uncollected cash.",
-  completed: false
-},
-{
-  id: 3,
-  title: "Order Release",
-  desciption: "Check out customers accounts and release or block orders accordingly.",
-  completed: true
-},
-{
-  id: 4,
-  title: "Weekly Reports",
-  desciption: "Sending the weekly reports for overdue invoices.",
-  completed: false  
-},
-]
+    id: 1,
+    title: "Call Clients",
+    description: "Call clients for overdue invoices.",
+    completed: true
+  },
+  {
+    id: 2,
+    title: "Dunning",
+    description: "Sending dunning letters to clients for uncollected cash.",
+    completed: false
+  },
+  {
+    id: 3,
+    title: "Order Release",
+    description: "Check out customers' accounts and release or block orders accordingly.",
+    completed: true
+  },
+  {
+    id: 4,
+    title: "Weekly Reports",
+    description: "Sending the weekly reports for overdue invoices.",
+    completed: false  
+  },
+];
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state= {
-      viewCompleted:false,
-      taskList:tasks
-
+    this.state = {
+      viewCompleted: false,
+      taskList: tasks
     };
   }
 
   displayCompleted = status => {
-    if (status) {
-      return this.setstatus({ viewCompleted: true })
-    }
-    return this.setstatus({ viewCompleted: false })
-  }
+    this.setState({ viewCompleted: status });
+  };
   
   renderTabList = () => {
     return (
@@ -54,29 +49,34 @@ class App extends Component {
           className={this.state.viewCompleted ? "active" : ""}
         >
           Completed
-            </span>
+        </span>
         <span
           onClick={() => this.displayCompleted(false)}
-          className={this.displayCompleted ? "" : "active"}
+          className={!this.state.viewCompleted ? "active" : ""}
         >
           Incompleted
-            </span>
+        </span>
       </div>
-    )
-  }
+    );
+  };
 
-// Rendering items in the list (completed || incompleted)
   renderItems = () => {
     const { viewCompleted } = this.state;
     const newItems = this.state.taskList.filter(
-      item => item.completed ==viewCompleted
+      item => item.completed === viewCompleted
     );
-  
+
     return newItems.map(item => (
-      <li key={item.id} className="list-group-item d-flex justify-content-between 
-      align-items-center">
-        <span className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""}`}
-          title={item.title}>
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={`todo-title mr-2 ${
+            this.state.viewCompleted ? "completed-todo" : ""
+          }`}
+          title={item.title}
+        >
           {item.title}
         </span>
         <span>
@@ -84,15 +84,13 @@ class App extends Component {
           <button className="btn btn-danger mr-2">Delete</button>
         </span>
       </li>
-    ))
+    ));
   };
-
-
 
   render() {
     return (
       <main className="context">
-        <h1 className="text-black text-uppercase text-center my-4"> Task Manager </h1>
+        <h1 className="text-black text-uppercase text-center my-4">Task Manager</h1>
         <div className="row">
           <div className="col-md-6 col-sma-10 mx-auto p-0">
             <div className="card p-3">
@@ -104,11 +102,10 @@ class App extends Component {
                 {this.renderItems()}
               </ul>
             </div>
-
           </div>
         </div>
       </main>
-    )
+    );
   }
 }
 
