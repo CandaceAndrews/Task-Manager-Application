8deab875f90e1fc836931d0bad21fdc0cda41a75
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-06-20 17:37:03 -0400

    modal.js

---
+++ frontend/src/components/modal.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Form,
+    FormGroup,
+    Input,
+    Label
+} from 'reactstrap';
